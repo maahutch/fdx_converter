c3b74f3582d9c03e5d35047b4c348ea082f38ca9
maahutch <mahutch@gmail.com>
2021-02-03 14:51:29 -0500

    adding requirements to readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # fdx_converter
 convert fdx screenplay to shooting script
+
+Required: 
+
+- Scene Number
+- Int/Ext
+- Location & First Line
+- Day/Night
+- Characters
+- Location (if assigned)
+- Stripboard (sequence)
--- read_fdx.py
+++ read_fdx.py
@@ -1,9 +1,32 @@
-import xml.etree.ElementTree as ET
-import csv
+from lxml import etree
 
-tree = ET.parse('C:/Users/maahutch/OneDrive/NoDuh2.1-Copy.fdx')
+tree = etree.parse(('noDuh.fdx'))
 
-    # get root element
-root = tree.getroot()
+root=tree.getroot()
 
-print(root)
+# get root element
+#root = tree.getroot()
+
+#print(root.tag)
+
+#for child in root:
+ #   print(child.tag, child.attrib)
+    
+#[elem.tag for elem in root.iter()]
+
+
+#for Content in root.iter('Content'):
+#    print(Content.attrib)
+    
+    
+
+
+#for paragraph in root.findall("./Content/Paragraph[@Type=Scene Heading"):
+   # x = paragraph.attrib
+   #if x['Type'] == 'Scene Heading':
+#   print(x)
+   
+
+x= root.xpath("./Content/Paragraph[@Type='Scene Heading']/child::*")
+      
+print(x[7].attrib)
